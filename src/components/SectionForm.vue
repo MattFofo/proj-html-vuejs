c6970423eabf2b80410bb87de1f629dfb78ebec6
MattFofo <matteo.segattini2@gmail.com>
2022-04-04 14:40:15 +0200

    sistemato layout form

--- src/components/SectionForm.vue
+++ src/components/SectionForm.vue
@@ -18,48 +18,52 @@
           <p>We will respond to your message as soon as possible.</p>
         </div>
         <!-- form -->
-        <form class="row g-3">
-          <div class="col-8">
-            <div class="row">
-              <div class="col-6">
-                <label for="name" class="form-label w-100">
-                  <input type="name" class="form-control" id="name"
-                  placeholder="Name">
-                </label>
+        <div class="col-8">
+          <form class="row">
+            <div class="col-8 w-100">
+              <div class="row">
+                <div class="col-6">
+                  <label for="name" class="form-label w-100">
+                    <input type="name" class="form-control" id="name"
+                    placeholder="Name"
+                    v-model="objFormSubmit.name">
+                  </label>
+                </div>
+                <div class="col-6">
+                  <label for="email" class="form-label w-100">
+                    <input type="email" class="form-control" id="email"
+                    placeholder="Email"
+                    v-model="objFormSubmit.email">
+                  </label>
+                </div>
+                <div class="col-6">
+                  <label for="phone" class="form-label w-100">
+                    <input type="phone" class="form-control" id="number"
+                    placeholder="Phone"
+                    v-model="objFormSubmit.phone">
+                  </label>
+                </div>
+                <div class="col-6">
+                  <label for="inputMoreInfo" class="form-label w-100">
+                    <select id="inputMoreInfo" class="form-select"
+                    v-model="objFormSubmit.moreInfo">
+                      <option selected>More Info</option>
+                      <option>...</option>
+                    </select>
+                  </label>
+                </div>
+                <div class="col-12">
+                  <label for="inputMessage" class="form-label w-100">
+                    <textarea type="text" class="form-control w-100" id="inputMessage"
+                    placeholder="Message" rows="12" v-model="objFormSubmit.message"></textarea>
+                  </label>
+                </div>
               </div>
-              <div class="col-6">
-                <label for="email" class="form-label w-100">
-                  <input type="email" class="form-control" id="email"
-                  placeholder="Email">
-                </label>
-              </div>
-              <div class="col-6">
-                <label for="phone" class="form-label w-100">
-                  <input type="phone" class="form-control" id="number"
-                  placeholder="Phone">
-                </label>
-              </div>
-              <div class="col-6">
-                <label for="inputMoreInfo" class="form-label w-100">
-                  <select id="inputMoreInfo" class="form-select">
-                    <option selected>More Info</option>
-                    <option>...</option>
-                  </select>
-                </label>
-              </div>
-              <div class="col-12">
-                <label for="inputMessage" class="form-label w-100">
-                  <textarea type="text" class="form-control w-100" id="inputMessage"
-                  placeholder="Message" rows="12"></textarea>
-                </label>
-              </div>
-              <div class="col-12">
-                <button type="submit" class="btn btn-primary">Send</button>
-              </div>
-
+              <button @click.prevent='formSubmit()'
+              type="submit" class="btn btn-primary form_button">Send</button>
             </div>
-          </div>
-        </form>
+          </form>
+        </div>
         <!-- right content -->
         <div class="col-4 position-absolute">
           <div class="right_content">
@@ -89,9 +93,7 @@
           </div>
           <button class="btn btn-outline-primary">VIEW MAP</button>
         </div>
-
       </div>
-
     </div>
   </section>
 </template>
@@ -99,6 +101,30 @@
 <script>
 export default {
   name: 'SectionForm',
+  data() {
+    return {
+      objFormSubmit: {
+        name: '',
+        email: '',
+        phone: '',
+        moreInfo: 'More Info',
+        message: '',
+      },
+      arrFormData: [],
+    };
+  },
+  methods: {
+    formSubmit() {
+      this.arrFormData.push(this.objFormSubmit);
+      this.objFormSubmit = {
+        name: '',
+        email: '',
+        phone: '',
+        moreInfo: 'More Info',
+        message: '',
+      };
+    },
+  },
 };
 </script>
 
@@ -112,6 +138,15 @@ export default {
     }
     p {
       font-size: 1.1rem;
+    }
+    form {
+      height: 25%;
+      #inputMessage {
+        height: 180px;
+      }
+      .form_button {
+        width: 50px;
+      }
     }
   }
   .col-4.position-absolute {
