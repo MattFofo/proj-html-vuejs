d355f153f748550deef5e1ea5da865f13b69fb65
MattFofo <matteo.segattini2@gmail.com>
2022-04-05 19:32:08 +0200

    bottone scroll page top con max width

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,13 @@
     <CargoHeader />
     <CargoMain />
     <CargoFooter />
-    <div @click="scrollPageTop" @keyup="scrollTop = 0" class="scroll_top" id="scroll_top">
+    <!-- <div @click="scrollPageTop" @keyup="scrollTop = 0" class="scroll_top" id="scroll_top">
       <font-awesome-icon icon="fa-solid fa-arrow-up" />
+    </div> -->
+    <div class="fixed">
+      <div @click="scrollPageTop" @keyup="scrollTop = 0" class="content scroll_page">
+        <font-awesome-icon icon="fa-solid fa-arrow-up" />
+      </div>
     </div>
   </div>
 </template>
@@ -35,28 +40,8 @@ export default {
   @import 'bootstrap';
   @import './assets/styles/style.scss';
 
-  .scroll_top {
-    position: fixed;
-    padding: 1rem;
-    margin: 1rem;
-    right: 0;
-    bottom: 0;
-    z-index: 200;
-    width: 4rem;
-    height: 4rem;
-    border-radius: 50%;
-    background-color: $color--text1;
-    max-width: 1980px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    opacity: .2;
-    cursor: pointer;
-    .fa-arrow-up {
-      font-size: 1.4rem;
-    }
-  }
-  .scroll_top:hover {
-    opacity: 1;
+  #app {
+    width: 1980px;
+    margin: auto;
   }
 </style>
