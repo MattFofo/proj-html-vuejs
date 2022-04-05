b383b8391a67bd94f6a859f3e3fc2830e1081435
MattFofo <matteo.segattini2@gmail.com>
2022-04-05 12:50:18 +0200

    stile sezione 4

--- src/components/SectionType4.vue
+++ src/components/SectionType4.vue
@@ -3,17 +3,26 @@
     <div class="container">
       <div class="row align-content-center text-center">
         <div class="col-12">
-          <small>pretitolo</small>
-          <h1>TITOLO TITOLO</h1>
+          <small class="pretitle">WHAT ARE WE DOING</small>
+          <h2 class="title">
+            <div class="bg_word d-inline-block">
+              <span class="text-white">
+                Results
+              </span>
+            </div>
+            <span class="text-white">
+              in Numbers
+            </span>
+          </h2>
         </div>
       </div>
       <div class="row row_cards position-relative">
-        <div v-for="i in 4" :key="i" class="col">
+        <div v-for="card in arrCards" :key="card.id" class="col">
           <div class="card position-relative text-center">
             <div class="icon_box m-auto">
-              <h3>234</h3>
+              <h3>{{ card.title }}</h3>
             </div>
-            <h4 class="py-3">titolo titolo</h4>
+            <h4 class="py-1">{{ card.p }}</h4>
           </div>
         </div>
       </div>
@@ -24,6 +33,32 @@
 <script>
 export default {
   name: 'SectionType4',
+  data() {
+    return {
+      arrCards: [
+        {
+          id: 1,
+          title: '128',
+          p: 'Certifications',
+        },
+        {
+          id: 2,
+          title: '230',
+          p: 'Employees',
+        },
+        {
+          id: 3,
+          title: '517',
+          p: 'Customers',
+        },
+        {
+          id: 4,
+          title: '94',
+          p: 'Countries Served',
+        },
+      ],
+    };
+  },
 };
 </script>
 
@@ -38,10 +73,17 @@ export default {
     padding-bottom: $height--header;
     max-width: 1980px;
     margin: auto;
+    h2{
+      margin-bottom: 7rem;
+    }
   }
   .card {
     border: none;
     background-color: transparent;
-
+    h3 {
+      color: $primary;
+      font-size: 2rem;
+      font-weight: 700;
+    }
   }
 </style>
